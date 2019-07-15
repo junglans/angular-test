6b3a25ea5e9144cafafe026c75b3ab96bce41b72
junglans <cderegistro@gmail.com>
2019-07-15 14:41:05 +0200

    Refactorización del código

--- src/app/components/medicos.component.spec.ts
+++ src/app/components/medicos.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from "@angular/core/testing";
+import { TestBed, async, ComponentFixture } from "@angular/core/testing";
 import { MedicosComponent } from './medicos.component';
 import { MedicosService } from '../services/medicos.service';
 import { HttpModule } from '@angular/http';
@@ -7,56 +7,49 @@ import { from, empty, throwError } from 'rxjs';
 
 describe('MedicosComponent', () => {
 
+    let component: MedicosComponent;
+    let fixture : ComponentFixture<MedicosComponent>;
+    let medicosService : MedicosService;
+
     beforeEach( async(() => {
         TestBed.configureTestingModule({
             declarations: [MedicosComponent],
             imports: [ HttpModule ],
             providers: [HttpClient, HttpHandler, MedicosService ]
         }).compileComponents();
+
+        fixture = TestBed.createComponent(MedicosComponent);
+        medicosService = TestBed.get(MedicosService);
+        component = fixture.debugElement.componentInstance;
     }));
 
     it ('Los componentes se ha creado correctamente', () => {
-        const fixture = TestBed.createComponent(MedicosComponent);
-        const app = fixture.debugElement.componentInstance;
-        expect(app).toBeTruthy();
+        expect(component).toBeTruthy();
     });
 
     it ('El componente debe inicializarse con una lista de médicos', () => {
-
-        const medicosService = TestBed.get(MedicosService);
 
         spyOn(medicosService, 'getMedicos').and.callFake(() => {
           return from(['Medico1', 'Medico2']);
         });
 
-        const fixture = TestBed.createComponent(MedicosComponent);
-        const app = fixture.debugElement.componentInstance;
         fixture.detectChanges();
 
-        expect(app.medicos.length).toBeGreaterThan(0);
+        expect(component.medicos.length).toBeGreaterThan(0);
     });
 
     it ('Debe llamar al método agregarMedico del componente', () => {
-        const medicosService = TestBed.get(MedicosService);
-
+        
         const spy = spyOn(medicosService, 'agregarMedico').and.callFake((medico) => {
             return empty(medico);
         })
-
-        const fixture = TestBed.createComponent(MedicosComponent);
-        const app = fixture.debugElement.componentInstance;
-        app.agregarMedico();
-
+ 
+        component.agregarMedico();
         expect(spy).toHaveBeenCalled();
     });
 
     it ('Debe agregar un nueve médico a la lista', () => {
-
-        const fixture = TestBed.createComponent(MedicosComponent);
-        const component = fixture.debugElement.componentInstance;
-
-        const medicosService = TestBed.get(MedicosService);
-
+      
         const medico = {id:1 , nombre: 'Juan'};
        
         spyOn(medicosService, 'agregarMedico').and.returnValue(
@@ -69,12 +62,6 @@ describe('MedicosComponent', () => {
     });
 
     it ('Si se produce un error, el error del componente debe ser igual al error del servicio.', () => {
-
-            const fixture = TestBed.createComponent(MedicosComponent);
-            const component = fixture.debugElement.componentInstance;
-
-            const medicosService = TestBed.get(MedicosService);
-
             const errMsg = 'Error agregando médico';
             spyOn(medicosService, 'agregarMedico').and.returnValue(
                 throwError(errMsg)
@@ -88,18 +75,10 @@ describe('MedicosComponent', () => {
     it ('Comprobar que el usuario aceptó borrar el médico', () => {
 
         spyOn(window, 'confirm').and.returnValue(true);
-        
-        const fixture = TestBed.createComponent(MedicosComponent);
-        const component = fixture.debugElement.componentInstance;
 
-        const medicosService = TestBed.get(MedicosService);
         const id = '1';
-        const spy = spyOn(medicosService, 'borrarMedico').and.returnValue( id => {
-            return empty();
-        });
-
+        const spy = spyOn(medicosService, 'borrarMedico').and.returnValue( empty());
         component.borrarMedico(id);
-
         expect(spy).toHaveBeenCalledWith(id);
     });
 });
